27c06ea8923addaa01550e9cbb97ecff481d86d2
VitorWolf <45910969+VitorWolf24@users.noreply.github.com>
2019-04-20 14:07:52 -0300

    Update prisma_daisy.mk

--- prisma_daisy.mk
+++ prisma_daisy.mk
@@ -7,7 +7,7 @@
 $(call inherit-product, device/xiaomi/daisy/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/prisma/config/common_full_phone.mk)
 
 # PE stuff
 TARGET_GAPPS_ARCH := arm64
